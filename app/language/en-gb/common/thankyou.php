054b7dc8626ff7f839b92ca5d6852cee1a0d9b8f
WALLReTuRn-new <71545886+WALLReTuRn-new@users.noreply.github.com>
2023-07-09 19:51:27 +0200

    Delete

--- app/language/en-gb/common/thankyou.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
- * WebSiteToYou License
- * Each line should be prefixed with  * 
- */
-$_['title'] = 'Thank You !';
-$_['description'] = 'Thank you for your interest, we will contact you as soon as possible.If you have more questions, please use the contact form.';
